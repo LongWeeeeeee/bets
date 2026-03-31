b4e663290aa7e014c6cfcda7c2058e6acec20644
alex <believer89793@yandex.com>
2026-03-31 22:02:26 +0300

    Restore accidentally deleted tracked files

---
+++ base/test.py
@@ -0,0 +1,7 @@
+# from maps_research import merge_temp_files_by_size
+# merged_files = merge_temp_files_by_size(
+#         mkdir='/Users/alex/Documents/ingame/bets_data/analise_pub_matches',
+#         max_size_mb=500,
+#         cleanup=False  # Удаляем temp_files после объединения
+#     )
+# Mid thresholds
